cc08343a9b39831826d599da22130bdb554a89a4
Mike Nabil <maikel_nabil@yahoo.com>
2017-03-21 13:23:17 -0400

    added routes

--- index.js
+++ index.js
@@ -29,4 +29,45 @@ app.listen(app.get("port"), function(){
   console.log("It's aliiive!");
 });
 
+
+
 //Routes
+
+//Index
+app.get("/api/presidents", function(req, res){
+  President.find({}).then(function(presidents){
+    res.json(presidents)
+  });
+});
+
+//Show
+app.get("/api/presidents/:name", function(req, res){
+  President.findOne({name: req.params.name}).then(function(president){
+    res.json(president)
+  });
+});
+
+//New
+app.post("/api/presidents", function(req, res){
+  President.create(req.body).then(function(president){
+    res.json(president)
+  })
+});
+
+//Delete
+app.delete("/api/presidents/:name", function(req, res){
+  President.findOneAndRemove({name: req.params.name}).then(function(){
+    res.json({ success: true })
+  });
+});
+
+//Update
+app.put("/api/presidents/:name", function(req, res){
+  President.findOneAndUpdate({name: req.params.name}, req.body, {new: true}).then(function(president){
+    res.json(president)
+  });
+});
+
+app.get("/*", function(req, res){
+  res.render("presidents");
+});
