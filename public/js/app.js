a74515adeb9f0e22e51e2eff4c975d0fa6cba834
Mike Nabil <maikel_nabil@yahoo.com>
2017-03-21 13:32:51 -0400

    added basic angular functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,38 @@
 angular
   .module("usPresidents", ["ui.router", "ngResource"])
+  .config(["$stateProvider", "$locationProvider", "$urlRouterProvider", Router])
+  .factory("President", ["$resource", President])
+  .controller("indexCtrl", ["$state", "President", indexController])
+  .controller("showCtrl", ["$state", "$stateParams", "President", showController])
+
+  function Router ($stateProvider, $locationProvider, $urlRouterProvider) {
+    $locationProvider.html5Mode(true)
+    $stateProvider
+      .state("index", {
+        url: "/",
+        templateUrl: "/assets/js/ng-views/index.html",
+        controller: "indexCtrl",
+        controllerAs: "vm"
+      })
+      .state("show", {
+        url: "/:name",
+        templateUrl: "/assets/js/ng-views/show.html",
+        controller: "showCtrl",
+        controllerAs: "vm"
+      })
+    $urlRouterProvider.otherwise("/")
+  }
+
+  function President ($resource) {
+    return $resource("/api/presidents/:name", {}, {
+      update: { method: "PUT" }
+    });
+  }
+
+  function indexController ($state, President) {
+
+  }
+
+  function showController ($state, $stateParams, President) {
+
+  }
